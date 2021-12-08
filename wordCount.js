6e8457a4ebf2eb268056398f411ebf7e2297879d
Sam Flavin <46271125+samflavin@users.noreply.github.com>
2021-12-08 13:58:15 -0600

    Create wordCount.js
    Also did this one in codesandbox.io

---
+++ wordCount.js
@@ -0,0 +1,29 @@
+let text = "car: carpet as java: javascript!!&@$%^&";
+
+function getWordArray(text) {
+  let textWithoutPunctuation = text.replace(/[^a-zA-Z0-9 \n\r+]/gim, "");
+  let wordArray = textWithoutPunctuation.match(/\b\w+\b/gim);
+
+  return getWordCount(wordArray);
+}
+
+function getWordCount(wordArray) {
+  let countedWords = [];
+  for (let x = 0; x < wordArray.length; x++) {
+    let count = 0;
+    for (let y = 0; y < wordArray.length; y++) {
+      if (wordArray[x] === wordArray[y]) {
+        count += 1;
+      }
+    }
+    countedWords.push(wordArray[x] + ":" + count);
+  }
+
+  let wordCountArrayNoDuplicates = countedWords.filter((c, index) => {
+    return countedWords.indexOf(c) === index;
+  });
+
+  return wordCountArrayNoDuplicates;
+}
+
+console.log(getWordArray(text));
