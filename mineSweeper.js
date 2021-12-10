dd3a5baf8bafa166b3e54618f9b36206b2d85a50
Sam Flavin <46271125+samflavin@users.noreply.github.com>
2021-12-10 11:54:21 -0600

    Update mineSweeper.js
    updated to match test suite

--- mineSweeper.js
+++ mineSweeper.js
@@ -1,16 +1,9 @@
-let puzzle = ["  *  ", "  *  ", "*****", "  *  ", "  *  "];
-
-annotate(puzzle);
-
-function annotate(puzzle) {
+export const annotate = (puzzle) => {
   let concatonatedArrays = [];
   for (let j = 0; j < puzzle.length; j++) {
     concatonatedArrays.push(puzzle[j].split(""));
   }
-  return solvePuzzle(concatonatedArrays);
-}
 
-function solvePuzzle(concatonatedArrays) {
   for (let x = 0; x < concatonatedArrays.length; x++) {
     for (let y = 0; y < concatonatedArrays[0].length; y++) {
       let count = 0;
@@ -77,26 +70,14 @@ function solvePuzzle(concatonatedArrays) {
       if (count > 0) {
         concatonatedArrays[x][y] = count;
       } else if (concatonatedArrays[x][y] !== "*") {
-        concatonatedArrays[x][y] = ".";
+        concatonatedArrays[x][y] = " ";
       }
     }
   }
+  let formattedSolution = [];
+  for (let z = 0; z < concatonatedArrays.length; z++) {
+    formattedSolution.push(concatonatedArrays[z].join(""));
+  }
+  return formattedSolution;
+};
 
-  return arraysToString(concatonatedArrays);
-}
-
-function arraysToString(arrays) {
-  let modified = arrays.join().replaceAll(",", "");
-  let width = arrays.length;
-
-  const wrap = (str, width) => {
-    var letters = [],
-      i = 0;
-    while (i < str.length) {
-      letters.push(str.slice(i, (i += width)));
-    }
-    return letters.join("\n");
-  };
-  return wrap(modified, width);
-}
-console.log(annotate(puzzle));
